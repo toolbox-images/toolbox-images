175ac71d28ad42aa13c5b69e9883d8fb47a88fb3
Jens Petersen <petersen@redhat.com>
2021-08-24 02:10:52 +0800

    fedora/make-image.sh: add toolbox create

--- images/fedora/make-image.sh
+++ images/fedora/make-image.sh
@@ -6,4 +6,8 @@ if [ ! -f Dockerfile ]; then
 fi
 podman pull fedora:36
 # remove dnf clean
+# todo: check if image exists
 buildah bud -t fedora-toolbox:36
+
+# todo: check if container exists
+toolbox create -i localhost/fedora-toolbox:36 -c fedora-toolbox-36
