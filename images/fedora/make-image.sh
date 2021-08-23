8c8b3d71d58d01e0b49148c036086f44883a4819
Jens Petersen <petersen@redhat.com>
2021-08-24 02:06:03 +0800

    fedora/make-image.sh: check for Dockerfile and pull

--- images/fedora/make-image.sh
+++ images/fedora/make-image.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-git clone https://src.fedoraproject.org/container/fedora-toolbox.git
-
-cd fedora-toolbox
-
+if [ ! -f Dockerfile ]; then
+  git clone https://src.fedoraproject.org/container/fedora-toolbox.git
+  cd fedora-toolbox
+fi
+podman pull fedora:36
+# remove dnf clean
 buildah bud -t fedora-toolbox:36
