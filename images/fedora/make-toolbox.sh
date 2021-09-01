bd60fe01b869ed2e44b0f36b2ed5652938565b4d
Jens Petersen <petersen@redhat.com>
2021-09-01 16:55:54 +0800

    fedora/make-toolbox.sh: use $1 for version

--- images/fedora/make-toolbox.sh
+++ images/fedora/make-toolbox.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+VERSION=${1:-36}
+
 if [ ! -f Dockerfile ]; then
   git clone https://src.fedoraproject.org/container/fedora-toolbox.git
   cd fedora-toolbox
+  if [ -n "$1" -a "$1" -lt 36 ]; then
+      git switch f$VERSION
+  fi
 fi
-podman pull fedora:36
+podman pull fedora:$VERSION
 # remove dnf clean
 # todo: check if image exists
-buildah bud -t fedora-toolbox:36
+buildah bud -t fedora-toolbox:$VERSION
 
 # todo: check if container exists
-toolbox create -i localhost/fedora-toolbox:36 -c fedora-toolbox-36
+toolbox create -i localhost/fedora-toolbox:$VERSION -c fedora-toolbox-$VERSION
